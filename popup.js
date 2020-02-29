071e59125ec78770bb2a1166f57724f9a2ebcfb5
Davi <davicpinheiro123@hotmail.com>
2020-02-29 14:48:11 -0300

    Extension User Interface

---
+++ popup.js
@@ -0,0 +1,8 @@
+function decodeVideo() {
+    console.log(document.querySelector("div"))
+}
+
+
+document.querySelector("#decode-btn").addEventListener("click", () => {
+    console.log(document.querySelector("div"))
+})
